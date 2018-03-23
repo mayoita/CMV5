271fb8a3e74e20a7cb156c22f90f8d6cc2378d7e
Massimo Moro <mayoita@hotmail.com>
2018-03-23 17:07:06 +0100

    Last

---
+++ AnnotazioneHome.swift
@@ -0,0 +1,21 @@
+//
+//  AnnotazioneHome.swift
+//  Casinò di Venezia
+//
+//  Created by Massimo Moro on 21/03/18.
+//  Copyright © 2018 Casinò di Venezia SPA. All rights reserved.
+//
+
+import UIKit
+
+class AnnotazioneHome: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SezioneHome.swift
@@ -0,0 +1,13 @@
+//
+//  SezioneHome.swift
+//  Casinò di Venezia
+//
+//  Created by Massimo Moro on 21/03/18.
+//  Copyright © 2018 Casinò di Venezia SPA. All rights reserved.
+//
+
+import UIKit
+
+class SezioneHome: UICollectionViewCell {
+    
+}
